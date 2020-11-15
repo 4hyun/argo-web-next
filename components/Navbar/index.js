fd447fdfdd12cfcd592cecfd2c53683d60756634
hsh <4hyun8@gmail.com>
2020-11-15 20:20:42 +0900

    remove unused deps

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import Hamburger from "components/Navbar/Hamburger";
 import LightLogo from "../../public/logos/lettermark/light.svg";
 
