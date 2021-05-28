4b41d6921ad179b03b8f9df94866a02f6bec0f02
Sa Hyun Hwang <4hyun8@gmail.com>
2021-05-28 17:02:53 +0900

    rebuild

--- configs/index.js
+++ configs/index.js
@@ -1,18 +1,18 @@
 export const BASE_URL = process.env.NODE_ENV === "production" ? process.env.PROD_CMS_BASE_URL : process.env.DEV_CMS_BASE_URL
 
 export const STRAPI_AUTH_TOKEN = process.env.NODE_ENV === "production" ? process.env.PROD_STRAPI_AUTH : process.env.DEV_STRAPI_AUTH
-const headers = { Authorization: `Bearer ${STRAPI_AUTH_TOKEN}` }
+export const headers = { Authorization: `Bearer ${STRAPI_AUTH_TOKEN}` }
 
 export const getStrapiURL = (path = "") => {
   console.log(">>DEBUG: BASE_URL: ", BASE_URL)
   return `${BASE_URL}${path}`
 }
 
-export const fetchStrapi = async (path) => {
-  const requestUrl = getStrapiURL(path)
-  const response = await fetch(requestUrl, headers)
-  const data = await response.json()
-  return data
-}
+// export const fetchStrapi = async (path) => {
+//   const requestUrl = getStrapiURL(path)
+//   const response = await fetch(requestUrl, headers)
+//   const data = await response.json()
+//   return data
+// }
 
 export const MENU_EID = "argo-elastic-menu"
