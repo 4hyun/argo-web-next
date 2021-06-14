3fe1e628dd74d20b871e27573eec682427e149c0
Sa Hyun Hwang <4hyun8@gmail.com>
2021-06-14 12:41:37 +0900

    remove /out/

--- components/Navbar/Hamburger.js
+++ components/Navbar/Hamburger.js
@@ -2,7 +2,7 @@ import React from "react"
 import tw from "twin.macro"
 import { Twirl as TwirlBurger } from "hamburger-react"
 
-const StyledBurger = tw(TwirlBurger)`text-argo-blue-600`
+const StyledBurger = tw(TwirlBurger)`text-argo-blue-600 select-none`
 
 const Hamburger = ({ toggled, toggle, closeMenu }) => {
   return <StyledBurger color="#250FAB" toggled={toggled} toggle={toggle}></StyledBurger>
