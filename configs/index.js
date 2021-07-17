a1be82cb3c3081f978a927ed6ca636503d367bbe
Sa Hyun Hwang <4hyun8@gmail.com>
2021-07-17 19:04:52 +0900

    test netlify build

--- configs/index.js
+++ configs/index.js
@@ -1,6 +1,6 @@
 export const BASE_URL = process.env.NODE_ENV === "production" ? process.env.PROD_CMS_BASE_URL : process.env.DEV_CMS_BASE_URL
 
-export const STRAPI_AUTH_TOKEN = process.env.NODE_ENV === "production" ? process.env.PROD_STRAPI_AUTH : process.env.DEV_STRAPI_AUTH
+// export const STRAPI_AUTH_TOKEN = process.env.NODE_ENV === "production" ? process.env.PROD_STRAPI_AUTH : process.env.DEV_STRAPI_AUTH
 
 export const getStrapiURL = (path = "") => {
   return `${BASE_URL}${path}`
