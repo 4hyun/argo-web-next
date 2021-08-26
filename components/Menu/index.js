c69c37d5a744afc023ea0963402fd411b7ec7e86
Sa Hyun Hwang <4hyun8@gmail.com>
2021-08-26 21:09:03 +0900

    update bg color

--- components/Menu/index.js
+++ components/Menu/index.js
@@ -8,10 +8,7 @@ import { default as menuData } from "./data"
 import { logoCircleDarkUrl } from "components/Brand"
 
 const StyledMenu = styled(SlidingMenu)`
-  ${tw`w-screen! md:(w-3/12)! bg-off-white`}
-  ${
-    "" /* background: center / 100% 130% linear-gradient( 161deg,hsl(255deg 100% 27%) 17%,hsl(218deg 100% 34%) 36%,hsl(224deg 97% 36% / 88%) 66%,hsl(177deg 100% 50%) 100% ); */
-  }
+  ${tw`w-screen! md:(w-3/12)! bg-argo-blue-700`}
 `
 
 const MenuHeader = styled.div`
@@ -30,7 +27,7 @@ export const LogoCircleDark = memo(styled.div`
 
 const CloseButton = memo(
   styled(Close)`
-    ${tw`cursor-pointer text-argo-blue-400 fill-current my-auto w-6 h-6`}
+    ${tw`cursor-pointer text-off-white fill-current my-auto w-6 h-6`}
   `
 )
 
@@ -48,7 +45,7 @@ const MenuItemA = styled.a`
     margin-left: var(--margin-offset);
     margin-right: var(--margin-offset);
   }
-  ${tw`flex text-2xl rounded-md text-argo-blue-400 font-bold px-2 py-2 lg:(active:(transform translate-y-1) transform transition-transform hover:(scale-105) text-3xl px-6 py-4)`}
+  ${tw`flex text-2xl rounded-md text-off-white font-bold px-2 py-2 lg:(active:(transform translate-y-1) transform transition-transform hover:(scale-105) text-3xl px-6 py-4)`}
   & > svg {
     ${tw`my-auto`}
   }
