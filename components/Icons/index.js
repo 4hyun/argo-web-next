9382c5592323ccc9ecb65758dc3e48fb88764c52
Sa Hyun Hwang <4hyun8@gmail.com>
2022-03-25 21:12:58 +0900

    Merge branch 'master' of https://github.com/4hyun/argo-web-next

--- components/Icons/index.js
+++ components/Icons/index.js
@@ -1,12 +1,21 @@
 // import * as faBrands from "@styled-icons/fa-brands";
 // import * as faRegular from "@styled-icons/fa-regular";
-import { At } from "@styled-icons/fa-solid/At"
-import { Close } from "@styled-icons/zondicons/Close"
-import { Check } from "@styled-icons/boxicons-regular/Check"
-import { UpArrowCircle } from "@styled-icons/boxicons-regular/UpArrowCircle"
-import { RightTopArrowCircle } from "@styled-icons/boxicons-regular/RightTopArrowCircle"
-import { ArrowLeftCircle } from "@styled-icons/bootstrap/ArrowLeftCircle"
-import { Open as OpenLinkArrow } from "@styled-icons/fluentui-system-regular/Open"
+import { At } from '@styled-icons/fa-solid/At';
+import { Close } from '@styled-icons/zondicons/Close';
+import { Check } from '@styled-icons/boxicons-regular/Check';
+import { UpArrowCircle } from '@styled-icons/boxicons-regular/UpArrowCircle';
+// import { RightTopArrowCircle } from '@styled-icons/boxicons-regular/RightTopArrowCircle';
+import { ArrowLeftCircle } from '@styled-icons/bootstrap/ArrowLeftCircle';
+import { Open as OpenLinkArrow } from '@styled-icons/fluentui-system-regular/Open';
+import { Search } from '@styled-icons/fa-solid/Search';
 // import { Close } from "@styled-icons/material/Close"
 
-export { At, Close, Check, UpArrowCircle, ArrowLeftCircle, OpenLinkArrow }
+export {
+  At,
+  Close,
+  Check,
+  UpArrowCircle,
+  ArrowLeftCircle,
+  OpenLinkArrow,
+  Search,
+};
