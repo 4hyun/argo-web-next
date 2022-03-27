b88fd8d61b557f403ba9333cc66ec7a660aabdd6
Sa Hyun Hwang <4hyun8@gmail.com>
2022-03-27 16:37:52 +0900

    deps: use lodash

--- hooks/useKeyBinding.js
+++ hooks/useKeyBinding.js
@@ -3,9 +3,10 @@ import { useState, useEffect } from 'react';
 const defaultConfig = {};
 
 const useKeyBinding = (ref, config) => {
+  /* add throttle to handler */
   /* initialize event listener on document */
   const keydownHandler = e => {
-    console.log('>>DEBUG keyDownHandler() e: ', e);
+    console.log('>>DEBUG keyDownHandler() e.code: ', e.code);
   };
 
   /* remove event listener */
