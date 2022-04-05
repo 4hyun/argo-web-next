b47108827f425915274bb0d61377f1a4c44ee311
Sa Hyun Hwang <4hyun8@gmail.com>
2022-04-06 08:10:45 +0900

    add Tyk Handbook to menu

--- data/menu.js
+++ data/menu.js
@@ -1,18 +1,29 @@
 const menuData = [
   {
     pid: 0,
-    item: { mid: 1, title: 'Home', slug: '/', icon: null, target: null },
+    item: { mid: 1, title: 'Home', url: '/', icon: null, target: null },
   },
   {
     pid: 0,
-    item: { mid: 2, title: 'Blog', slug: '/blog', icon: null, target: null },
+    item: { mid: 2, title: 'Blog', url: '/blog', icon: null, target: null },
+  },
+
+  {
+    pid: 0,
+    item: {
+      mid: 4,
+      title: 'Tyk Handbook',
+      url: '/tyk-handbook',
+      icon: 'openLinkArrow',
+      target: '_blank',
+    },
   },
   {
     pid: 0,
     item: {
       mid: 3,
       title: 'Tyk',
-      slug: 'https://tyk.io/',
+      url: 'https://tyk.io/',
       icon: 'openLinkArrow',
       target: '_blank',
     },
