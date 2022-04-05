ee19cae9b8609e2ffe2b136e3ba793ac052f21e2
Sa Hyun Hwang <4hyun8@gmail.com>
2022-04-06 08:12:46 +0900

    use `styled` imported from one package = `twin.macro`

--- components/Menu/index.jsx
+++ components/Menu/index.jsx
@@ -1,7 +1,6 @@
 import { memo } from 'react';
 // import { motion } from "framer-motion"
-import styled from 'styled-components';
-import tw from 'twin.macro';
+import tw, { styled } from 'twin.macro';
 import { slide as MenuSlideBox } from 'react-burger-menu';
 import { Close } from 'components/Icons';
 import { ELASTIC_MENU_EID } from 'configs';
