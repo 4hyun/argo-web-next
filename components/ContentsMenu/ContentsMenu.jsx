66d5e46f0c0d42bd64923275071d9593d7243224
Sa Hyun Hwang <4hyun8@gmail.com>
2022-04-06 13:28:18 +0900

    refactor: add dyanamic Item component support for RecurseList

--- components/ContentsMenu/ContentsMenu.jsx
+++ components/ContentsMenu/ContentsMenu.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import MenuRoot from './MenuRoot';
-import { RecurseList } from './RecurseList';
+import RecurseList from './RecurseList';
+import { Item } from './Item';
 
 const KEY_FIELD = 'menuItemUid';
 const getKeyProp = prop => prop[KEY_FIELD];
@@ -15,40 +16,10 @@ const ContentMenu = ({ data, sortKey = 'sort' }) => {
         <RecurseList
           key={j}
           data={data}
-          getKeyProp={getKeyProp} />
+          getKeyProp={getKeyProp}
+          itemAs={Item}
+        />
       ))}
-      {/* {(sortKey ? data.sort((a, b) => a[sortKey] - b[sortKey]) : data).map(
-        rootMenuItem =>
-          rootMenuItem?.items.length > 0 ? (
-            <RecurseListDeprecated
-              key={rootMenuItem.id}
-              keyField={KEY_FIELD}
-              {...rootMenuItem}
-            >
-              {(sortKey
-                ? rootMenuItem.items.sort((a, b) => a[sortKey] - b[sortKey])
-                : rootMenuItem.items
-              ).map(item =>
-                item?.items?.length > 0 ? (
-                  <RecurseListDeprecated
-                    key={item.id}
-                    keyField={KEY_FIELD}
-                    sortKey={deepSort && sortKey}
-                    {...item}
-                  />
-                ) : (
-                  <Item
-                    key={item.menuItemUid}
-                    {...item} />
-                ),
-              )}
-            </RecurseListDeprecated>
-          ) : (
-            <Item
-              key={rootMenuItem.menuItemUid}
-              {...rootMenuItem} />
-          ),
-      )} */}
     </MenuRoot>
   );
 };
