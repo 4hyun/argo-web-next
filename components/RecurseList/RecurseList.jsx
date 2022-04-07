0c6643aa91fb08f486ff7fe11d4d50fc207bf52e
Sa Hyun Hwang <4hyun8@gmail.com>
2022-04-07 10:36:12 +0900

    refactor: sort-asc util into separate file & simplify <Sorted Data> conditionals

--- components/RecurseList/RecurseList.jsx
+++ components/RecurseList/RecurseList.jsx
@@ -1,31 +1,20 @@
 import { UL } from '@/components/Base';
-
-export const getSortProps = props => {
-  const { keyField, sortKey } = props;
-  return { keyField, sortKey };
-};
+import sortAsc from '@/lib/utils/sort-asc.js';
 
 const RecurseList = props => {
-  const { getKeyProp = () => 'undefined', sortKey, data } = props;
+  const { cssProp, getKeyProp = () => 'undefined', sortKey, data } = props;
 
+  const sortedItems = sortAsc(data.items, sortKey);
   const Item = props.itemAs;
 
-  let sortedDataItems;
-  if (data.items) {
-    if (sortKey) {
-      sortedDataItems = data.items.sort((a, b) => a[sortKey] - b[sortKey]);
-    } else {
-      sortedDataItems = data.items;
-    }
-  }
-
   return (
-    <UL>
+    <UL
+      css={cssProp}>
       <Item
         key={`${getKeyProp}-root`}
         data={data}></Item>
-      {sortedDataItems &&
-        sortedDataItems.map((item, index) => (
+      {sortedItems &&
+        sortedItems.map((item, index) => (
           <RecurseList
             key={index}
             data={item}
