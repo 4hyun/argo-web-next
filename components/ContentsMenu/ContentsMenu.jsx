6c60475e4685713ce2ccf08d9362f5910099e260
Sa Hyun Hwang <4hyun8@gmail.com>
2022-04-07 10:36:33 +0900

    use sort-asc util

--- components/ContentsMenu/ContentsMenu.jsx
+++ components/ContentsMenu/ContentsMenu.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
+import tw, { css, styled } from 'twin.macro';
 import MenuRoot from './MenuRoot';
 import RecurseList from './RecurseList';
 import { Item } from './Item';
+import sortAsc from '@/lib/utils/sort-asc.js';
 
 const KEY_FIELD = 'menuItemUid';
 const getKeyProp = prop => prop[KEY_FIELD];
 
+const RecurseListDevStyles = css`
+  li:hover {
+    background: red;
+    ${tw`cursor-pointer`}
+  }
+  li:active {
+    ${tw`ring-4 ring-red-400`}
+  }
+`;
+
 const ContentMenu = ({ data, sortKey = 'sort' }) => {
-  const sortedData = sortKey
-    ? data.sort((a, b) => a[sortKey] - b[sortKey])
-    : data;
+  const sortedData = sortAsc(data, sortKey);
+  if (!sortedData) return null;
   return (
     <MenuRoot>
       {sortedData.map((data, j) => (
         <RecurseList
+          cssProp={RecurseListDevStyles}
           key={j}
           data={data}
           getKeyProp={getKeyProp}
